7a6ae9f8e9abbe4fd748ceb357d4624a547581b8
denit <denit@DESKTOP-U3BIU13>
2022-10-25 17:45:53 +0300

    Modified names

--- Models/Author.cs
+++ Models/Author.cs
@@ -6,9 +6,11 @@ namespace Tomi_Lavinia_Lab2.Models
     public class Author
     {
         public int ID { get; set; }
-        [Display(Name = "Author Name")]
+        [Display(Name = "First Name")]
         public string   FirstName { get; set; }
+        [Display(Name = "Last Name")]
         public string   LastName { get; set; }
+        [Display(Name = "Author Name")]
         public string FullName { get { return FirstName + " " + LastName; } }
 
         public ICollection<Book>? Books { get; set; }
--- Models/Publisher.cs
+++ Models/Publisher.cs
@@ -1,8 +1,13 @@
-﻿namespace Tomi_Lavinia_Lab2.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace Tomi_Lavinia_Lab2.Models
 {
     public class Publisher
     {
         public int ID { get; set; }
+
+        [Display(Name = "Publisher Name")]
         public string PublisherName { get; set; }
         public ICollection<Book>? Books { get; set; }
     }
