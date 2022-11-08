9120631b9b6aff8498e93dd9481acec391c46524
denit <denit@DESKTOP-U3BIU13>
2022-11-08 19:01:38 +0200

    Lab5 Part

--- Migrations/LibraryIdentity/20221108165643_CreateIdentity2.cs
+++ Migrations/LibraryIdentity/20221108165643_CreateIdentity2.cs
@@ -6,7 +6,7 @@ namespace Tomi_Lavinia_Lab2.Migrations.LibraryIdentity
 {
     public partial class CreateIdentity2 : Migration
     {
-        protected override void Up(MigrationBuilder migrationBuilder)
+        protected override void Up(MigrationBuilder  migrationBuilder)
         {
 
         }
